a78adac702862c28b0f25b07e46799cb5cb8153e
riomabox <riomabox@gmail.com>
2015-12-15 15:09:04 +0700

    Begin

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+ 
+app.use(express.static(__dirname + '/public'));
+ 
+app.get('/', function(req, res){
+  res.render('/index.html');
+});
+ 
+server.listen(8080);
+console.log("Multiplayer app listening on port 8080");
+
