59ae094fea7c1efa4310bf99d7cf689c67845d96
riomabox <riomabox@gmail.com>
2016-01-03 08:54:53 +0700

    Lanjut lagi, masih error

--- public/src/game.js
+++ public/src/game.js
@@ -5,29 +5,61 @@ var Q = Quintus({audioSupported: [ 'wav','mp3' ]})
       .controls().touch();
  
 Q.gravityY = 0;
+
+require(['socket.io/socket.io.js']);
+
+var players = [];
+var socket = io.connect('http://localhost:8080');
+var UiPlayers = document.getElementById("players");
+var selfId, player;
  
 var objectFiles = [
   './src/player'
 ];
  
 require(objectFiles, function () {
-  Q.scene('arena', function (stage) {
-    stage.collisionLayer(new Q.TileLayer({ dataAsset: '/maps/arena.json', sheet: 'tiles' }));
- 
-    var player = stage.insert(new Q.Player({ x: 50, y: 100 }));
-    stage.add('viewport').follow(player);
-  });
- 
-  var files = [
-    '/images/tiles.png',
+	function setUp (stage) {
+		socket.on('count', function (data) {
+			UiPlayers.innerHTML = 'Players: ' + data['playerCount'];
+		});
+		socket.on('connected', function (data) {
+			selfId = data['playerId'];
+			player = new Q.Player({ playerId: selfId, x: 50, y: 50, socket: socket });
+			stage.insert(player);
+			stage.add('viewport').follow(player);
+		});
+	}
+	
+	socket.on('updated', function (data) {
+		var actor = players.filter(function (obj) {
+			return obj.playerId == data['playerId'];
+		})[0];
+		if (actor) {
+			actor.player.p.x = data['x'];
+			actor.player.p.y = data['y'];
+			actor.player.p.sheet = data['sheet'];
+			actor.player.p.update = true;
+		} else {
+			var temp = new Q.Actor({ playerId: data['playerId'], x: data['x'], y: data['y'], sheet: data['sheet'] });
+			players.push({ player: temp, playerId: data['playerId'] });
+			stage.insert(temp);
+		}
+	});
+	Q.scene('arena', function (stage) {
+		stage.collisionLayer(new Q.TileLayer({ dataAsset: '/maps/arena.json', sheet: 'tiles' }));
+		setUp(stage);
+	});
+	
+	var files = [
+	'/images/tiles.png',
     '/maps/arena.json',
     '/images/sprites.png',
     '/images/sprites.json'
-  ];
- 
-  Q.load(files.join(','), function () {
-    Q.sheet('tiles', '/images/tiles.png', { tilew: 32, tileh: 32 });
-    Q.compileSheets('/images/sprites.png', '/images/sprites.json');
-    Q.stageScene('arena', 0);
-  });
+	];
+	
+	Q.load(files.join(','), function () {
+		Q.sheet('tiles', '/images/tiles.png', { tilew: 32, tileh: 32 });
+		Q.compileSheets('/images/sprites.png', '/images/sprites.json');
+		Q.stageScene('arena', 0);
+	});
 });
--- public/src/player.js
+++ public/src/player.js
@@ -1,20 +1,37 @@
-require([], function () { 
-  Q.Sprite.extend('Player', {
-    init: function (p) {
-      this._super(p, {
-        sheet: 'player'
-      });
+require([], function () {
+	Q.Sprite.extend('Actor', {
+		init: function (p) {
+			this._super(p, {
+				update: true
+			});
+			
+			var temp = this;
+			setInterval(function () {
+				if (!temp.p.update) {
+					temp.destroy();
+				}
+				temp.p.update = false;
+			}, 3000);
+		}
+	});
+	
+	Q.Sprite.extend('Player', {
+		init: function (p) {
+			this._super(p, {
+				sheet: 'player'
+			});
  
       this.add('2d, platformerControls, animation');
     },
-    step: function (dt) {
-      if (Q.inputs['up']) {
-        this.p.vy = -200;
-      } else if (Q.inputs['down']) {
-        this.p.vy = 200;
-      } else if (!Q.inputs['down'] && !Q.inputs['up']) {
-        this.p.vy = 0;
-      }
-    }
-  });
+	step: function (dt) {
+			if (Q.inputs['up']) {
+				this.p.vy = -200;
+			} else if (Q.inputs['down']) {
+				this.p.vy = 200;
+			} else if (!Q.inputs['down'] && !Q.inputs['up']) {
+				this.p.vy = 0;
+			}
+			this.p.socket.emit('update', { playerId: this.p.playerId, x: this.p.x, y: this.p.y, sheet: this.p.sheet });
+		}
+	});
 });
